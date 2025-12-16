962485ecc933ecf2892d3aec28ede52fc6cdc115
Jackson Ledezma <jacksonledezma01@gmail.com>
2025-12-15 19:34:00 -0500

    Remove IoT module and related files
    Deleted the iot.controller.ts, iot.module.ts, and iot.service.ts files from the backend IoT module. This removes all IoT-related API endpoints and in-memory state management for sensors, camera, and LED control.

--- backend/src/module/iot/iot.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { IotService } from './iot.service';
-import { IotController } from './iot.controller';
-
-@Module({
-    controllers: [IotController],
-    providers: [IotService],
-})
-export class IotModule { }
